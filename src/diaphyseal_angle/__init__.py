321e5858de4acd1206f5b4fcacb5a80ef646ab1f
MARYIA VOL <146164676+mariamvol@users.noreply.github.com>
2026-02-14 22:12:47 +0300

    Delete src/diaphyseal_angle directory

--- src/diaphyseal_angle/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ["run_inference"]
-__version__ = "0.1.0"
-
-from .pipeline import run_inference
--- src/diaphyseal_angle/io.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import numpy as np
-
-def load_image_rgb(path: str) -> np.ndarray:
-    img = Image.open(path).convert("RGB")
-    return np.array(img)
-
-def save_image_rgb(path: str, img_rgb: np.ndarray) -> None:
-    Image.fromarray(img_rgb).save(path)
